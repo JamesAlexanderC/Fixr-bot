73d251a229ad631a0100489097109df3174fe466
James Clarke <james.clarke@amulethotkey.com>
2026-08-12 21:04:38 +0100

    all logging for engine is completed - yayy!

--- engine.py
+++ engine.py
@@ -14,6 +14,7 @@ import json
 import uuid
 import asyncio
 from camoufox.async_api import AsyncCamoufox
+import os
 
 import scan_loop
 import get_ticket
@@ -53,6 +54,7 @@ async def login_get_ticket(
     except Exception as e:
         logger.critical("Error starting Camoufox (this will be a pain to debug): %s, ABORTING", str(e), exc_info=True)
         raise e
+    global ticket_sessions
     ticket_sessions[session_name]["camoufox"] = camoufox
     ticket_sessions[session_name]["browser"] = browser
     ticket_sessions[session_name]["page"] = page
@@ -74,9 +76,26 @@ async def login_get_ticket(
         )
         logger.debug("Reserved ticket for account %s successfully", email)
     except Exception as e:
-        logger.error("Error getting in on %s account: %s", email, str(e), exc_info=True)
+        logger.error("Error getting ticket on %s account: %s", email, str(e), exc_info=True)
         raise e
     await inputQueue.put(f"buy-ticket|{session_name}")
+
+async def clean_camoufox(session: dict):
+    if not isinstance(session, dict):
+        raise TypeError("'session' must be a dict")
+    if not all([session.get("task"), session.get("camoufox")]):
+        raise ValueError("Missing required fields: task, camoufox")
+    try:
+        session["task"].cancel()
+        await session["camoufox"].__aexit__(
+            None, 
+            None, 
+            None
+        )
+        session = {}
+    except Exception as e:
+        raise Exception(f"Error cancelling camoufox session: {str(e)}") from e
+
 
 # --------------------------------------------------------------------
 # Main Queue handler - basically takes messages from the queue,
@@ -88,25 +107,35 @@ async def queueHandler():
     while not stopEvent.is_set():
         msg = await inputQueue.get()
         logger.info("Recieved message: %s", msg)
-        logger.critical("TEST")
+
         if msg == "start-scan-loop":
-            logger.debug("Message recongised as 'start-scan-loop'")
+            logger.info("Message recongised as 'start-scan-loop'")
             if scan_session != {}:
                 logger.warning("Scan is already active, IGNORING")
-            try:
-                # currently assuming the accounts file exists with at least one entry - I will change this
-                with open("accounts.json", "r") as f:
-                    accounts = json.load(f)
-                account = list(accounts.keys())[0]
-                logger.debug("Accounts file loaded successfully")
-            except Exception as e:
-                logger.error("Error loading accounts: %s, ABORTING", str(e), exc_info=True)
                 continue
             try:
-                # same as above with assuming file exists
-                with open("event.json", "r") as f:
-                    event = json.load(f)
-                logger.debug("Event file loaded successfully")
+                if os.path.exists("accounts.json"):
+                    with open("accounts.json", "r") as f:
+                        accounts = json.load(f)
+                    if len(accounts) == 0:
+                        logger.error("Error, no accounts to scan with, ABORTING")
+                        continue
+                    account = list(accounts.keys())[0]
+                else:
+                    with open("accounts.json", "w") as f:
+                        json.dump({}, f)
+                logger.debug("Accounts file loaded successfully")
+            except Exception as e:
+                logger.error("Error loading accounts: %s, ABORTING (hint: is there invalid JSON in 'accounts.json'?)", str(e), exc_info=True)
+                continue
+            try:
+                if os.path.exists("event.json"):
+                    with open("event.json", "r") as f:
+                        event = json.load(f)
+                    if not all([event.get("organiser_url"), event.get("ticket_keyword"), event.get("scan_interval")]):
+                        logger.error("Error, incomplete event data, ABORTING")
+                        continue
+                    logger.debug("Event file loaded successfully")
             except Exception as e:
                 logger.error("Error loading event: %s, ABORTING", str(e), exc_info=True)
                 continue
@@ -127,7 +156,12 @@ async def queueHandler():
                 logger.debug("Logged in successfully")
             except Exception as e:
                 logger.error("Error during login: %s, ABORTING", str(e), exc_info=True)
-                continue
+                try: 
+                    await camoufox.__aexit__()
+                except Exception as e:
+                    logger.critical("Failed to stop coroutine and Camoufox instance: %s, ABORTING", str(e), exc_info=True)
+                finally:
+                    continue
             try:
                 task = asyncio.create_task(
                     scan_loop.run(
@@ -138,10 +172,17 @@ async def queueHandler():
                         inputQueue=inputQueue,
                     )
                 )
+                scan_session = {"camoufox": camoufox, "browser": browser, "task": task}
                 logger.debug("Created scan task successfully")
             except Exception as e:
                 logger.error("Error creating scan task: %s, ABORTING", str(e), exc_info=True)
-            scan_session = {"camoufox": camoufox, "browser": browser, "task": task}
+                try:
+                    await clean_camoufox(scan_session)
+                except Exception as e:
+                    logger.critical("Failed to stop coroutine and Camoufox instance: %s, ABORTING", str(e), exc_info=True)
+                finally:
+                    continue
+
         elif msg == "stop-scan-loop":
             logger.info("Message is recognised as 'stop-scan-loop'")
             if scan_session == {}:
@@ -158,6 +199,7 @@ async def queueHandler():
                 logger.critical("Failed to stop coroutine and Camoufox instance: %s, ABORTING", str(e), exc_info=True)
                 continue
             scan_session = {}
+
         elif msg.split("|")[0] == "get-tickets":
             logger.info("Message is recognised as 'get-tickets'")
             try: 
@@ -192,35 +234,65 @@ async def queueHandler():
                             url=msg.split("|")[1],
                         )
                     )
-                    ticket_sessions[session_name]= {"task": task}
+                    # This relies on running before the coroutine above, which is nondeterministic (could cause problems in future)
+                    ticket_sessions[session_name] = {"task": task}
                     logger.info("Created get ticket task for %s successfully", account)
                 except Exception as e:
-                    logger.error("Error creating ticket task for %s: %s", account, str(e), exc_info=True)
+                    logger.error("Error creating ticket task for %s: %s, ABORTING", account, str(e), exc_info=True)
+                    try:
+                        await clean_camoufox(ticket_sessions[session_name])
+                    except Exception as e:
+                        logger.critical("Failed to stop coroutine and Camoufox instance: %s, ABORTING", str(e), exc_info=True)
+                    finally:
+                        continue
+
         elif msg.split("|")[0] == "buy-ticket":
             logger.info("Message is recognised as 'buy-tickets'")
-            # I rlly need to sort this out
             try:
                 camoufox = ticket_sessions[msg.split("|")[1]]["camoufox"]
                 page = ticket_sessions[msg.split("|")[1]]["page"]
+                logger.debug("Message parsed successully: %s", msg.split("|"))
             except Exception as e:
                 logger.error("Error parsing 'buy-tickets' message: %s, ABORTING", str(e), exc_info=True)
                 continue
             try:
                 with open("card.json", "r") as f:
                     card = json.load(f)
+                if not all([card.get("cardNumber"), card.get("cardExpiry"), card.get("cardCvc"), card.get("cardPostcode")]):
+                    logger.error("Error, incomplete card data, ABORTING")
+                    continue
+                logger.debug("Card loaded successfully")
             except Exception as e:
                 logger.error("Error loading card: %s, ABORTING", str(e), exc_info=True)
-            # NEED TO EDIT AND TEST NEW VERSION OF BUY-TICKETS THEN INSERT HERE, THEN CAN RESUME LOGGING
-            await checkout.buy(
-                page=page,
-            )
-            url = ticket_sessions[msg.split("|")[1]]["page"].url
-            ticket_sessions[msg.split("|")[1]].cancel()
-            ticket_sessions[msg.split("|")[1]]["camoufox"].__aexit__(
-                None,
-                None,
-                None,
-            )
+                continue
+            try:
+                await checkout.buy(
+                    page=page,
+                    cnumber=card.get("cardNumber"),
+                    expiry=card.get("cardExpiry"),
+                    cvc=card.get("cardCvc"),
+                    postal=card.get("cardPostcode"),
+                )
+                logger.info("Ticket purchase completed successfully")
+            except Exception as e:
+                logger.error("Error during ticket purchase: %s, ABORTING", str(e), exc_info=True)
+                try:
+                    await clean_camoufox(ticket_sessions[msg.split("|")[1]])
+                except Exception as e:
+                    logger.critical("Failed to stop coroutine and Camoufox instance: %s, ABORTING", str(e), exc_info=True)
+                finally:
+                    continue
+            try:
+                ticket_sessions[msg.split("|")[1]]["task"].cancel()
+                await ticket_sessions[msg.split("|")[1]]["camoufox"].__aexit__(
+                    None,
+                    None,
+                    None,
+                )
+            except Exception as e:
+                logger.critical("Failed to stop coroutine and Camoufox instance: %s, ABORTING", str(e), exc_info=True)
+                continue
+
         else:
             logger.warning("No known action for message: %s, IGNORING", msg)
             continue
